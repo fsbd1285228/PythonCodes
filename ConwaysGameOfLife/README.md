96e1ed29aa9a40766aad8eb8cc08ecf1610380f4
Rui WANG <jack.wangrui@outlook.com>
2017-07-11 21:48:37 +0800

    Decorator annotation update Sample code for Conway's game of life update (with README.md)
    Signed-off-by: Rui WANG <jack.wangrui@outlook.com>

---
+++ ConwaysGameOfLife/README.md
@@ -0,0 +1,5 @@
+# ConwaysGameOfLife
+
+References:
+1.https://www.shiyanlou.com/courses/running
+2.Conway's Game of Life in Python https://jakevdp.github.io/blog/2013/08/07/conways-game-of-life/
--- PythonStarter/Decorator_class.py
+++ PythonStarter/Decorator_class.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 def decorator(aClass):
-    class newClass:
+    class newClass:  # Returns a new class newClass in the decorator
         def __init__(self, age):
             self.total_display   = 0
-            self.wrapped         = aClass(age)
+            self.wrapped         = aClass(age)  # Record the object in the original object
         def display(self):
-            self.total_display += 1
+            self.total_display += 1  # New property total_display was added in order to record the time calling display. Also changed the method 'display'
             print("total display", self.total_display)
             self.wrapped.display()
     return newClass
